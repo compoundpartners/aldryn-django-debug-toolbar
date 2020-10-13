bc37e1de02de247476bcbb301b493fac89f06874
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2020-10-13 15:23:24 +0200

    update django-debug-toolbar to 2.2

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 from aldryn_django_debug_toolbar import __version__
 
 setup(
-    name='aldryn-django-debug-toolbar',
+    name='js-django-debug-toolbar',
     version=__version__,
     description=open('README.rst').read(),
     author='Joseph Melettukunnel',
     author_email='joseph.melettukunnel@divio.ch',
     packages=find_packages(),
     platforms=['OS Independent'],
-    install_requires=["django-debug-toolbar==1.8"],
+    install_requires=["django-debug-toolbar==2.2"],
     include_package_data=True,
     zip_safe=False,
 )
